29b4195569c0fc092823c90b90bb1d8d50ef90af
Yordan Kostadinov <yordan.k@siteground.com>
2023-02-12 20:45:16 +0000

    Download and chmod the start.py

--- scripts/synapse/update-scripts.sh
+++ scripts/synapse/update-scripts.sh
@@ -2,5 +2,6 @@
 [ -d /data/templates ] || mkdir -p /data/templates
 project_base="https://raw.githubusercontent.com/xdancho/fly-matrix/main"
 curl -o /data/scripts/generate-config.sh -s "${project_base}/scripts/synapse/generate-config.sh"
+curl -o /data/scripts/start.py -s "${project_base}/scripts/synapse/start.py"
 curl -o /data/templates/homeserver.yaml -s "${project_base}/templates/homeserver.yaml"
-chmod +x /data/scripts/generate-config.sh
+chmod +x /data/scripts/generate-config.sh /data/scripts/start.py

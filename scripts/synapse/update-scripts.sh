1cc6764403a170fcd93b43a8b4a44a7371996606
Yordan Kostadinov <yordan.k@siteground.com>
2023-02-12 20:34:17 +0000

    Fix typo

--- scripts/synapse/update-scripts.sh
+++ scripts/synapse/update-scripts.sh
@@ -1,6 +1,6 @@
 [ -d /data/scripts ] || mkdir -p /data/scripts
 [ -d /data/templates ] || mkdir -p /data/templates
 project_base="https://raw.githubusercontent.com/xdancho/fly-matrix/main"
-curl -o /data/scripts/generate-config.sh -s "${synapse_scripts}/scripts/synapse/generate-config.sh"
-curl -o /data/templates/homeserver.yaml -s "${synapse_scripts}/templates/homeserver.yaml"
+curl -o /data/scripts/generate-config.sh -s "${project_base}/scripts/synapse/generate-config.sh"
+curl -o /data/templates/homeserver.yaml -s "${project_base}/templates/homeserver.yaml"
 chmod +x /data/scripts/generate-config.sh

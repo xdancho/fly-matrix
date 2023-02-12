dcd54d0cbfb9874b6e34b09acc23af6a792962f6
Yordan Kostadinov <yordan.k@siteground.com>
2023-02-12 20:26:21 +0000

    create template dir and pull synapse template to persisten storage

--- scripts/synapse/update-scripts.sh
+++ scripts/synapse/update-scripts.sh
@@ -1,4 +1,6 @@
 [ -d /data/scripts ] || mkdir -p /data/scripts
-synapse_scripts="https://raw.githubusercontent.com/xdancho/fly-matrix/main/scripts/synapse"
-curl -o /data/scripts/generate-config.sh -s "${synapse_scripts}/generate-config.sh"
+[ -d /data/templates ] || mkdir -p /data/templates
+project_base="https://raw.githubusercontent.com/xdancho/fly-matrix/main"
+curl -o /data/scripts/generate-config.sh -s "${synapse_scripts}/scripts/synapse/generate-config.sh"
+curl -o /data/templates/homeserver.yaml -s "${synapse_scripts}/templates/homeserver.yaml"
 chmod +x /data/scripts/generate-config.sh

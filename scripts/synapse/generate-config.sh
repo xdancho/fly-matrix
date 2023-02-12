0d1b970dc455746fc970a868a00804ad8ab56089
Yordan Kostadinov <yordan.k@siteground.com>
2023-02-12 20:54:59 +0000

    Fix start.py location in persisten storage

--- scripts/synapse/generate-config.sh
+++ scripts/synapse/generate-config.sh
@@ -3,5 +3,5 @@ if test -f /data/homeserver.yaml; then
     echo "Synapse homeserver config already exists"
     exit 0
 fi
-/data/scripts/synapse/start.py generate
-/data/scripts/synapse/start.py migrate_config
+/data/scripts/start.py generate
+/data/scripts/start.py migrate_config
